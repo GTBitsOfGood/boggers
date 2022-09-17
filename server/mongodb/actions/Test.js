fb30283bb729bb1d28bc760c1f367054c30113b7
Samarth Chandna <samarthchandna52@gmail.com>
2022-09-17 18:10:58 -0400

    added basic CRUD

--- server/mongodb/actions/Test.js
+++ server/mongodb/actions/Test.js
@@ -7,6 +7,34 @@ async function getTests() {
   return tests;
 }
 
+async function createTest(name) {
+  await connectMongo();
+  const newTest = await Test.create({ name });
+  return newTest;
+}
+
+async function updateTest(id, name) {
+  await connectMongo();
+  const updatedTest = await Test.findByIdAndUpdate(id, { name }, { new: true });
+  return updatedTest;
+}
+
+async function removeTest(id) {
+  await connectMongo();
+  const deletedTest = await Test.findByIdAndRemove(id);
+  return deletedTest;
+}
+
+async function removeTestByName(name) {
+  await connectMongo();
+  const deletedTest = await Test.findOneAndDelete({ name });
+  return deletedTest;
+}
+
 export {
-  getTests
+  getTests,
+  createTest,
+  updateTest,
+  removeTest,
+  removeTestByName,
 };
