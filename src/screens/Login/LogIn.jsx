87a4c7850629e741a0de0de7d16839b945ee31cb
tsun900 <tian.sun@outlook.com>
2022-09-19 17:55:21 -0400

    integrated prettier instead of prettier with Elinst

--- src/screens/Login/LogIn.jsx
+++ src/screens/Login/LogIn.jsx
@@ -1,32 +1,47 @@
 import React, { useState } from "react";
 
 export function LoginPage() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
-      <div>
-        <div style={{
+  return (
+    <div>
+      <div
+        style={{
           borderStyle: "none none solid",
-          marginBottom: "1rem"
-        }}>
-          <h1>Log In</h1>
-        </div>
-        <form style={{
+          marginBottom: "1rem",
+        }}
+      >
+        <h1>Log In</h1>
+      </div>
+      <form
+        style={{
           display: "flex",
           flexDirection: "column",
-          alignItems: "flex-start"
-        }}>
-          <label>
-            Email:
-            <input type="text" name="email" onChange={(event) => setEmail(event.target.value)}/>
-          </label>
-          <label>
-            Password:
-            <input type="password" id="pass" name="password" minLength={8} required onChange={(event) => setPassword(event.target.value)}/>
-          </label>
-          <input type="submit" value="Login"/>
-        </form>
-      </div>
-    )
+          alignItems: "flex-start",
+        }}
+      >
+        <label>
+          Email:
+          <input
+            type="text"
+            name="email"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            id="pass"
+            name="password"
+            minLength={8}
+            required
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
 }
