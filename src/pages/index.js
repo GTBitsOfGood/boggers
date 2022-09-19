661653a005216a4f3c1e1679908c9370d9df4c5a
tsun900 <tian.sun@outlook.com>
2022-09-19 18:03:56 -0400

    restoren initial version of index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import urls from '../../utils/urls';
 
 export default function Home() {
   return (
@@ -14,15 +13,15 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://www.notion.so/gtbitsofgood/Boggers-ab17249a75864e45a69e7e5a185f98dd">project bogger!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by <a href={urls.pages.login} style={{color: "blue"}}>Log in</a>
-          {/* <code className={styles.code}>pages/index.js</code> */}
+          Get started by editing{" "}
+          <code className={styles.code}>pages/index.js</code>
         </p>
 
-        {/* <div className={styles.grid}>
+        <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
@@ -50,7 +49,7 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
-        </div> */}
+        </div>
       </main>
 
       <footer className={styles.footer}>
