5b5db86b3d8268f80d2ec6adc4fe182821a915ec
tsun900 <tian.sun@outlook.com>
2022-09-29 18:07:40 -0400

    revert unneccessary changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import connectMongo from "../server/mongodb/connectMongo"
 
 export default function Home() {
   return (
