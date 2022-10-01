9032d3dd49d864ccd3ec400027dfc732b21d5177
tsun90 <52758623+tsun900@users.noreply.github.com>
2022-10-01 12:07:42 -0400

    Update urls.js

--- utils/urls.js
+++ utils/urls.js
@@ -1,9 +1,27 @@
+
+function getBaseURL () {
+    // if backend
+    if (process.env.VERCEL_URL) {
+      return `https://${process.env.VERCEL_URL}`
+    }
+    // if client-side
+    if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+      return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    }
+    return 'http://localhost:3000'
+  }
+
 export default {
-    base: 'http://localhost:3000',
+    base: getBaseURL(),
     dbUrl: process.env.MONGO_DB ?? 'mongodb://localhost:27017',
     dbName: process.env.DB_NAME ?? 'local',
     pages: {
         index: '/',
         login: '/login'
+    },
+    api: {
+        test: 'api/test',
+        login: 'api/login',
+        hello: 'api/hello'
     }
 }
