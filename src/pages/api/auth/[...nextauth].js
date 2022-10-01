71f497bb6b925d1020fbf2a4260e9f44d87371b2
David Pang <davidpang314@gmail.com>
2022-10-01 12:14:48 -0400

    add next auth

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,40 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { getUser } from "../../../../server/mongodb/actions/User";
+
+const authOptions = {
+  providers: [
+    CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+          email: { label: "Email", type: "text" },
+          password: { label: "Password", type: "password" }
+        },
+        async authorize(credentials, req) {
+          const user = await getUser(credentials.email);
+
+          if (user && await bcrypt.compare(credentials.password, user.password)) {
+            return user;
+          } else {
+            return null
+          }
+        }
+      })
+  ],
+  callbacks: {
+    async jwt({token, user}) {
+      if (user) token.user = user;
+      return token;
+    },
+    async session({session, token}) {
+      session.user = token.user;
+      return session;
+    },
+    async redirect({ url, baseUrl }) {
+        return "/members";
+    }
+  },
+}
+
+export default NextAuth(authOptions)
