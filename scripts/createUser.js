dff81a0f562b5bac52fd42e00e14f49dbb86e117
David Pang <davidpang314@gmail.com>
2022-10-01 13:03:28 -0400

    add script to create user

---
+++ scripts/createUser.js
@@ -0,0 +1,12 @@
+import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { createUser } from "../server/mongodb/actions/User";
+
+dotenv.config({
+    path: ".env.local",
+});
+
+createUser("root@boggers.com", "boggers", "root", true).then((user) => {
+    console.log(user);
+    mongoose.connection.close();
+});
