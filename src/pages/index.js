9f35de9de2915c332aa785620b379217ccbc40ca
tsun90 <52758623+tsun900@users.noreply.github.com>
2022-10-01 05:44:58 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Member from "./member";
+import connectMongo from "../../server/utils/connectMongo";
 export default function Home() {
   return (
     <div className={styles.container}>
