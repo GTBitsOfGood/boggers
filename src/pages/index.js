5cd2aded1d3bc4fca1b626581ed9451bb64b4171
tsun900 <tian.sun@outlook.com>
2022-10-01 05:16:38 -0400

    bugfix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Member from "./member";
-
+import { getTests } from "../server/mongodb/actions/Test";
 export default function Home() {
   return (
     <div className={styles.container}>
--- src/server/mongodb/connectMongo.js
+++ src/server/mongodb/connectMongo.js
@@ -4,17 +4,15 @@ import urls from "../../../utils/urls";
 let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { db: null, conn: null, promise: null };
+  cached = global.mongoose = { db: null, promise: null };
 }
-//cached mongodb connection but not database, please access database with following line:
-//const db = cached.conn.db(urls.dbName)
-async function connectMongo() {
+export default async function connectMongo() {
   if (cached.db) {
     return cached.db;
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(urls.dbUrl).then((mongoose) => {
+    cached.promise = mongoose.connect(urls.dbUrl, { dbName: urls.dbName }).then((mongoose) => {
       return mongoose;
     });
   }
@@ -28,5 +26,3 @@ async function connectMongo() {
   cached.db = cached.conn.db(urls.dbName);
   return cached.db;
 }
-
-export default connectMongo;
