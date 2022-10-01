c1693ab828ab4ea240c3903c72ab2297c79e5dd3
tsun90 <52758623+tsun900@users.noreply.github.com>
2022-10-01 08:14:29 -0400

    Enable top level await feature

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, options) => {
+    config.experiments = {
+      topLevelAwait: true,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
