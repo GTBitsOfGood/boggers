b5611565cee97d081bfc9024a75d173a3bed74ac
tsun90 <52758623+tsun900@users.noreply.github.com>
2022-10-03 03:59:27 -0400

    Update urls.js

--- utils/urls.js
+++ utils/urls.js
@@ -1,27 +1,27 @@
 //fetch Vercel baseurl based on current status
-function getBaseURL () {
-    // if Deployement
-    if (process.env.VERCEL_URL) {
-      return `https://${process.env.VERCEL_URL}`
-    }
-    // if Build Step
-    if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-      return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    }
-    return 'http://localhost:3000'
+function getBaseURL() {
+  // if Deployement
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
   }
+  // if Build Step
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  }
+  return "http://localhost:3000";
+}
 
 export default {
-    base: getBaseURL(),
-    dbUrl: 'mongodb://localhost:27017',
-    dbName: 'local',
-    pages: {
-        index: '/',
-        login: '/login'
-    },
-    api: {
-        test: 'api/test',
-        login: 'api/login',
-        hello: 'api/hello'
-    }
-}
+  base: getBaseURL(),
+  dbUrl: process.env.DB_URL ?? "mongodb://localhost:27017",
+  dbName: "local",
+  pages: {
+    index: "/",
+    login: "/login",
+  },
+  api: {
+    test: "api/test",
+    login: "api/login",
+    hello: "api/hello",
+  },
+};
