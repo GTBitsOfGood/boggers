624fc3dcce4f6c691e24e6b0de466a7a304bcc08
Jeffrey Deng <jeffreydeng@Jeffreys-MacBook-Pro-2.local>
2022-10-20 14:31:23 -0400

    fixed schema

--- server/mongodb/models/InitSchema.js
+++ server/mongodb/models/InitSchema.js
@@ -1,77 +1,67 @@
 import mongoose from "mongoose";
 
-const user = new mongoose.Schema({
-    phoneNumber: {
-        type: String,
-        required: true,
-    },
-    preferences: {
-        type: String,
-        required: true,
-    },
-    admin: {
-        type: Boolean,
-        default: false,
-        required: true,
-    }
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  password: String,
+  phoneNumber: String,
+  preference: String,
+  admin: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["Developer", "Alumni", "Senior Dev", "Dev Bootcamper"],
+  },
+  status: {
+    type: String,
+    enum: ["Active", "Inactive"],
+  },
 });
 
-const project = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    semesterCreated: {
-        type: String, 
-        enum: ['spring','summer','fall'],
-        required: true,
-    },
-    yearCreated: {
-        type: Number,
-        required: true,
-    }
+const tenureSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  semester: {
+    type: String,
+    enum: ["spring", "summer", "fall"],
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  project: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+    required: true,
+  },
 });
 
-const tenure = new mongoose.Schema({
-    userId: {
-        type: user,
-        required: true,
-    },
-    semester: {
-        type: String,
-        enum: ['spring', 'summer', 'fall'],
-        required: true,
-    },
-    year: {
-        type: Number,
-        required: true,
-    },
-    department: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        required: true,
-    },
-    projectId: {
-        type: project,
-        required: true,
-    },
-    notes: {
-        type: String,
-        required: true,
-    }
-});
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+const Tenure = mongoose.models.Tenure || mongoose.model("Tenure", tenureSchema);
 
-
-
-const userSchema = mongoose.model("userSchema", user);
-const projectSchema = mongoose.model("projectSchema", project);
-const tenureSchema = mongoose.model("tenureSchema", tenure);
-
-module.exports = {
-    userSchema: userSchema,
-    projectSchema: projectSchema,
-    tenureSchema: tenureSchema
-}
+export {User, Tenure};
