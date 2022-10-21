724ece25d0faa05b5a7958ebd9588ea230817ba2
tsun90 <tian.sun@outlook.com>
2022-10-21 10:31:51 -0400

    inplement styling for login page

--- src/screens/Login/LogIn.jsx
+++ src/screens/Login/LogIn.jsx
@@ -1,36 +1,41 @@
 import React, {useState} from "react";
+import classes from "./Login.module.css";
 
 export function LoginPage() {
   // eslint-disable-next-line no-unused-vars
   const [email, setEmail] = useState("");
   // eslint-disable-next-line no-unused-vars
   const [password, setPassword] = useState("");
-
   return (
-    <div>
-      <div
-        style={{
-          borderStyle: "none none solid",
-          marginBottom: "1rem",
-        }}>
-        <h1>Log In</h1>
+    <div className={classes.body}>
+      <div className={classes.baseContainer}>
+        <div>
+          <p className={classes.header}>Welcome to boggers</p>
+        </div>
+        <p className={classes.introduction}>Your Bits of Good Membership Platform</p>
+        <form className={classes.form} action="http://localhost:3000/api/auth/callback/credentials" method="POST">
+          <div className={classes.inputCountainer}>
+            <label className={classes.textStyle}>EMAIL:</label>
+            <input className={classes.inputBar} type="text" name="email" required onChange={(event) => setEmail(event.target.value)} />
+          </div>
+          <div className={classes.inputCountainer}>
+            <label className={classes.textStyle}>PASSWORD:</label>
+            <input
+              className={classes.inputBar}
+              type="password"
+              id="pass"
+              name="password"
+              minLength={8}
+              required
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <div className={classes.submission}>
+            <p className={classes.forgot}>Forgot password?</p>
+            <input className={classes.button} type="submit" value="Sign in" />
+          </div>
+        </form>
       </div>
-      <form
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-        }}>
-        <label>
-          Email:
-          <input type="text" name="email" onChange={(event) => setEmail(event.target.value)} />
-        </label>
-        <label>
-          Password:
-          <input type="password" id="pass" name="password" minLength={8} required onChange={(event) => setPassword(event.target.value)} />
-        </label>
-        <input type="submit" value="Login" />
-      </form>
     </div>
   );
 }
