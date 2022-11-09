07e30148b91cee4872313271cca9c9654287c9d5
luke9kim8 <luke9kim8@gmail.com>
2022-11-08 19:08:52 -0500

    Route to login

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import {CacheProvider} from "@emotion/react";
 import theme from "../styles/theme";
 import createEmotionCache from "../styles/createEmotionCache";
+import "../styles/styles.css";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
