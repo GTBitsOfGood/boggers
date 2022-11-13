3f4ee73e802111734f642f8d615f825c781fc7ce
Samarth Chandna <samarthchandna52@gmail.com>
2022-11-13 16:53:56 -0500

    added styling to footer inputs

--- src/screens/Members/FooterElement/FooterElement.js
+++ src/screens/Members/FooterElement/FooterElement.js
@@ -1,14 +1,19 @@
 import styles from "./FooterElement.module.css";
 
 export default function FooterElement({admin, title, state, setState}) {
+  const fieldInput =
+    title === "STATUS" ? (
+      <select className={styles.FooterElementInput} value={state} onChange={(e) => setState(e.target.value)}>
+        <option>Active</option>
+        <option>Inactive</option>
+      </select>
+    ) : (
+      <input className={styles.FooterElementInput} value={state} onChange={(e) => setState(e.target.value)} />
+    );
   return (
     <div className={styles.FooterElement}>
       <div className={styles.FooterElementTitle}>{title}</div>
-      {admin ? (
-        <input className={styles.FooterElementValue} value={state} onChange={(e) => setState(e.target.value)} />
-      ) : (
-        <div className={styles.FooterElementValue}>{state}</div>
-      )}
+      {admin ? fieldInput : <div className={styles.FooterElementValue}>{state}</div>}
     </div>
   );
 }
