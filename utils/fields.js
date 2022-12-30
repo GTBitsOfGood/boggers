56a3ee456ca2b5d3164541170e97546f377b7905
Samarth Chandna <samarthchandna52@gmail.com>
2022-12-30 14:03:19 +0400

    updated mongodb models to use field enums

--- utils/fields.js
+++ utils/fields.js
@@ -1,25 +1,29 @@
+const preferences = ["Front-end", "Back-end", "Full-stack"];
+
+const memberTypes = {
+  0: "Member",
+  1: "Admin",
+  2: "Root Admin",
+};
+
+const semesters = ["Spring", "Summer", "Fall"];
+
 const departments = ["Engineering"];
 
 const roles = ["Developer", "Senior Developer", "Associate Director", "Director", "Alumni"];
 
 const projects = ["Helping Mamas", "GT Scheduler", "Product Maintenance", "Exec"];
 
-const preferences = ["Front-end", "Back-end", "Full-stack"];
-
 const statuses = ["Active", "Inactive"];
 
-const memberTypes = {
-  0: "Member",
-  1: "Admin",
-};
-
 const fields = {
+  preferences,
+  memberTypes,
+  semesters,
   departments,
   roles,
   projects,
-  preferences,
   statuses,
-  memberTypes,
 };
 
 export default fields;
