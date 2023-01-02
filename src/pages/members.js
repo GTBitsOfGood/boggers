367361ed7c9f5d00479a8e4ca57745909d5c7608
Tony Kim <snowberry2375@gmail.com>
2023-01-03 02:07:07 +0900

    remove photo successfully, check default avatar import statement and scripts.js import statement

--- src/pages/members.js
+++ src/pages/members.js
@@ -4,14 +4,14 @@ import {getSession} from "next-auth/react";
 export async function getServerSideProps(context) {
   const session = await getSession(context);
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
+  // if (!session) {
+  //   return {
+  //     redirect: {
+  //       destination: "/",
+  //       permanent: false,
+  //     },
+  //   };
+  // }
 
   return {
     props: {session},
--- src/screens/Members/UploadPhotoModal/UploadPhotoModal.js
+++ src/screens/Members/UploadPhotoModal/UploadPhotoModal.js
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import styles from "./UploadPhotoModal.module.css";
+import Avatar from "src/public/Avatar.png";
 
 export default function UploadPhotoModal({displayModal, closeModal, setImageUrl, setImageBlob}) {
   const [uploading, setUploading] = useState(false);
@@ -14,6 +15,15 @@ export default function UploadPhotoModal({displayModal, closeModal, setImageUrl,
     new: {opacity: 1},
   };
 
+  const removePhotoHandler = (e) => {
+    e.preventDefault();
+    setUploading(true);
+    setImageBlob(null);
+    setImageUrl(Avatar.src);
+    setUploading(false);
+    closeModal();
+  };
+
   return (
     <div className={styles.Modal} style={displayModal ? modalTransition.new : modalTransition.original}>
       <div className={styles.ModalBox} style={displayModal ? modalBoxTransition.new : modalBoxTransition.original}>
@@ -21,33 +31,49 @@ export default function UploadPhotoModal({displayModal, closeModal, setImageUrl,
           &#10006;
         </div>
         <div className={styles.ModalHeader}>UPLOAD A PHOTO</div>
-        <label className={styles.ModalUploadButton} style={uploading ? {} : {cursor: "pointer"}}>
+        <div className={styles.ModalButtonContainer}>
+          <label className={styles.ModalUploadButton} style={uploading ? {} : {cursor: "pointer"}}>
+            {uploading ? (
+              "Uploading..."
+            ) : (
+              <>
+                <input
+                  type="file"
+                  style={{display: "none"}}
+                  accept="image/*"
+                  onChange={async (e) => {
+                    setUploading(true);
+                    const uploadedImage = e.target.files[0];
+                    if (!uploadedImage) {
+                      setUploading(false);
+                      return;
+                    }
+                    const imageUrl = URL.createObjectURL(uploadedImage);
+                    setImageBlob(uploadedImage);
+                    setImageUrl(imageUrl);
+                    console.log(uploadedImage);
+                    console.log(imageUrl);
+                    setUploading(false);
+                    closeModal();
+                  }}
+                />
+                Select File
+              </>
+            )}
+          </label>
+          {/* <label className={styles.ModalUploadButton} style={uploading ? {} : {cursor: "pointer"}}>
           {uploading ? (
             "Uploading..."
           ) : (
-            <>
-              <input
-                type="file"
-                style={{display: "none"}}
-                accept="image/*"
-                onChange={async (e) => {
-                  setUploading(true);
-                  const uploadedImage = e.target.files[0];
-                  if (!uploadedImage) {
-                    setUploading(false);
-                    return;
-                  }
-                  const imageUrl = URL.createObjectURL(uploadedImage);
-                  setImageBlob(uploadedImage);
-                  setImageUrl(imageUrl);
-                  setUploading(false);
-                  closeModal();
-                }}
-              />
-              Select File
-            </>
+          <>
+          <input/>
+          Remove Photo</>
           )}
-        </label>
+        </label> */}
+          <label className={styles.ModalUploadButton} onClick={removePhotoHandler}>
+            Remove Photo
+          </label>
+        </div>
       </div>
     </div>
   );
