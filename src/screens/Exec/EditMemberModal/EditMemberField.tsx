a839b03b8ba2c42fd509375ce5522126a36ad5d0
Samarth Chandna <samarthchandna52@gmail.com>
2023-01-05 18:42:09 +0400

    ran prettier

--- src/screens/Exec/EditMemberModal/EditMemberField.tsx
+++ src/screens/Exec/EditMemberModal/EditMemberField.tsx
@@ -17,7 +17,7 @@ const Label = ({ label }) => {
   );
 };
 
-const StyledSelect = styled(Select)(({ theme }) => ({
+const StyledSelect = styled(Select)(() => ({
   "& .MuiSelect-select": {
     height: "27px",
   },
@@ -28,18 +28,14 @@ const StyledSelect = styled(Select)(({ theme }) => ({
   },
 }));
 
-export default function EditMemberField({ label, type, state, setState, onChange, menu, keyFunc=(key)=>key }) {
+export default function EditMemberField({ label, type, state, setState, onChange, menu, keyFunc = (key) => key }) {
   onChange = onChange || ((e) => setState(e.target.value));
 
   return (
     <div style={{ margin: "0px 10px" }}>
       <Label label={label} />
       {type === "select" ? (
-        <StyledSelect
-          className={styles.select}
-          size="small"
-          value={state}
-          onChange={onChange}>
+        <StyledSelect className={styles.select} size="small" value={state} onChange={onChange}>
           {menu.map((key) => (
             <MenuItem className={styles.menuItem} key={keyFunc(key)} value={key}>
               <div className={`${styles.menuItemText} ${state === key ? styles.selected : styles.unselected}`}>{keyFunc(key)}</div>
