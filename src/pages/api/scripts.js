fb707aff929205bac86ce96e99b34e28e0ee6bda
Samarth Chandna <samarthchandna52@gmail.com>
2023-01-07 21:52:11 +0400

    changed scripts

--- src/pages/api/scripts.js
+++ src/pages/api/scripts.js
@@ -1,5 +1,7 @@
 import User from "../../server/mongodb/models/User";
 import Tenure from "../../server/mongodb/models/Tenure";
+import AccountRecovery from "../../server/mongodb/models/AccountRecovery";
+import EmailVerification from "../../server/mongodb/models/EmailVerification";
 import { createRootUser, createSeedUser } from "../../server/mongodb/actions/User";
 import requestWrapper from "../../server/utils/middleware";
 
@@ -8,6 +10,17 @@ async function handler(req, res) {
 
   switch (req.query.task) {
     case "seed": {
+      const user = await createRootUser();
+      return res.status(200).json(user);
+    }
+    case "seedCredentials": {
+      const { email, password, access } = req.query;
+      if (!email || !password) return res.status(400).json({ error: "Missing/Invalid email or password" });
+
+      const user = await createSeedUser(email, password, access);
+      return res.status(200).json(user);
+    }
+    case "deleteAll": {
       try {
         await User.collection.drop();
       } catch (error) {
@@ -18,15 +31,16 @@ async function handler(req, res) {
       } catch (error) {
         // empty
       }
-      const user = await createRootUser();
-      return res.status(200).json(user);
-    }
-    case "seedCredentials": {
-      const { email, password, access } = req.query;
-      if (!email || !password) return res.status(400).json({ error: "Missing/Invalid email or password" });
-
-      const user = await createSeedUser(email, password, access);
-      return res.status(200).json(user);
+      try {
+        await AccountRecovery.collection.drop();
+      } catch (error) {
+        // empty
+      }
+      try {
+        await EmailVerification.collection.drop();
+      } catch (error) {
+        // empty
+      }
     }
   }
 }
