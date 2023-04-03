22d6a4738f4b635287a857209944b234af800e8d
tsun90 <tian.sun@outlook.com>
2023-04-03 03:09:09 -0400

    fix eslint

---
+++ src/components/WIndowSize/index.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+export const GetWindowSize = () => {
+  const [WindowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", handleResize);
+      handleResize();
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []);
+  return WindowSize;
+};
